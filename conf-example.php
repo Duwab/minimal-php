3d193d0c67a5d4f27011ef386cc624449ff84b4c
Administrator <admin@duwab.com>
2014-10-29 22:47:56 +0100

    2

--- conf-example.php
+++ conf-example.php
@@ -3,9 +3,9 @@ $CONF = array(
 	"register_key" => "il faut une autorisation",
 	"database" => array(
 		"host"		=> "localhost",
-		"database"	=> "minimal",
-		"username"	=> "minimal",
-		"password"	=> "dbadminminimal"
+		"database"	=> "mydbname",
+		"username"	=> "user",
+		"password"	=> "secretpassword"
 	),
 	"session_lifetime" => 3600,
 	"cookie_lifetime" => 3600 * 24 * 31
