822ed88df5f9c58df74c84c0eac561ab6f0f7073
Administrator <admin@duwab.com>
2014-11-02 19:37:49 +0100

    init + D&D

--- application/home.php
+++ application/home.php
@@ -1,7 +1,21 @@
-<a href="/profile">Profile</a>
-<?php
-	if(isset($name))
-		echo $name;
-	if(isset($job))
-		echo $job;
-?>
+<h1>Home</h1>
+<ul id="items">
+    <li>item 1</li>
+    <li>item 2</li>
+    <li>item 3</li>
+</ul>
+<div style="width:400px;border-bottom:1px dotted black;margin:20px auto 30px"></div>
+<ul id="items" class="inline-demo">
+    <li>item 1</li>
+    <li>item 2</li>
+    <li>item 3</li>
+</ul>
+<script>
+$(document).on('ready', function(){
+	console.log('ready');
+	var el = document.getElementById('items');
+	new Sortable(el);
+	var el = document.getElementsByClassName('inline-demo')[0];
+	new Sortable(el);
+});
+</script>
--- views/template.php
+++ views/template.php
@@ -2,6 +2,7 @@
 <head>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	<script src="/assets/js/script.js"></script>
+	<script src="/assets/js/sortable.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="/assets/css/style.css" media="screen" /> 
 </head>
 <body>
